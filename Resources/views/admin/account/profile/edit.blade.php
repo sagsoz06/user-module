7588716123db58e4f82f223e17234f2ea2443f72
aniliso <tkaptan41@gmail.com>
2017-09-05 19:43:03 +0300

    Updating dynamic relationship

--- Resources/views/admin/account/profile/edit.blade.php
+++ Resources/views/admin/account/profile/edit.blade.php
@@ -60,11 +60,14 @@
 </div>
 {!! Form::close() !!}
 @stop
+
 @section('footer')
     <a data-toggle="modal" data-target="#keyboardShortcutsModal"><i class="fa fa-keyboard-o"></i></a> &nbsp;
 @stop
+
 @section('shortcuts')
 @stop
+
 @push('js-stack')
 <script>
 $( document ).ready(function() {
--- Resources/views/admin/roles/create.blade.php
+++ Resources/views/admin/roles/create.blade.php
@@ -52,15 +52,18 @@
 </div>
 {!! Form::close() !!}
 @stop
+
 @section('footer')
     <a data-toggle="modal" data-target="#keyboardShortcutsModal"><i class="fa fa-keyboard-o"></i></a> &nbsp;
 @stop
+
 @section('shortcuts')
     <dl class="dl-horizontal">
         <dt><code>b</code></dt>
         <dd>{{ trans('user::roles.navigation.back to index') }}</dd>
     </dl>
 @stop
+
 @push('js-stack')
 <script>
 $( document ).ready(function() {
